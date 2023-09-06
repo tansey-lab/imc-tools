fe4405d0865413b569dfda28f31352c602fc5211
jeffquinn-msk <quinnj2@mskcc.org>
2023-09-06 11:24:40 -0400

    initial commit

--- src/imc_tools/images.py
+++ src/imc_tools/images.py
@@ -1,12 +1,14 @@
 import tqdm
 from readimc import MCDFile
-from glob import glob
 import os.path
 from pathlib import Path
 from PIL import Image
 import numpy as np
+import logging
 
 from matplotlib import pyplot as plt
+
+logger = logging.getLogger(__name__)
 
 
 def remove_outliers(arr, percentile=99.5):
@@ -56,9 +58,20 @@ def plot_channels(mcd_fn, output_dir):
 
 
 def plot_channel_onto_panorama(mcd_file,
-                               output_file,
+                               output_dir,
                                panorama_idx=0,
+                               overlay_color=(57, 255, 20),
                                channel_label="DNA1"):
+    """
+    Plot a channel onto the panorama image
+
+    :param mcd_file: Input MCD file for IMC experiment
+    :param output_dir: Output dir where multiple images will be saved
+    :param panorama_idx: If the MCD file happens to contain multiple panoramas,
+                         this is the index of the panorama to use
+    :param overlay_color: Color to use for transparent overlay of the channel
+    :param channel_label: Name of channel label to plot
+    """
     with MCDFile(mcd_file) as f:
         for slide_idx, slide in enumerate(f.slides):
             instrument_panorama = [x for x in slide.panoramas if x.metadata["Type"] == "Instrument"][panorama_idx]
@@ -95,10 +108,9 @@ def plot_channel_onto_panorama(mcd_file,
             panorama_dims = panorama_arr.shape[:2]
 
             # convert x pixels to um
-            x_um_per_pixel = panorama_dims[0] / (max_x - min_x)
-            y_um_per_pixel = panorama_dims[1] / (max_y - min_y)
+            x_um_per_pixel = panorama_dims[1] / (max_x - min_x)
+            y_um_per_pixel = panorama_dims[0] / (max_y - min_y)
 
-            print(min_x, max_x, min_y, max_y, panorama_dims, x_um_per_pixel, y_um_per_pixel)
             background = Image.fromarray(panorama_arr).convert("RGBA")
             combined = background
             for acquisition_idx, acquisition in enumerate(slide.acquisitions):
@@ -114,20 +126,13 @@ def plot_channel_onto_panorama(mcd_file,
 
                 acquisition_arr = remove_outliers(img[channel_idx, ...])
 
-                """
-                “ROIEndXPosUm”	End X-coordinates of the region of interest (µm).
-                “ROIEndYPosUm”	End Y-coordinates of the region of interest (µm)
-                “ROIStartXPosUm”	Start X-coordinates of the region of interest (µm). Note: This value must be divided by 1000 to correct for a bug (missing decimal point) in the Fluidigm software.
-                “ROIStartYPosUm”	Start Y-coordinates of the region of interest (µm). Note: This value must be divided by 1000 to correct for a bug (missing decimal point) in the Fluidigm software.
-                """
-
+                # Source: https://software.docs.hubmapconsortium.org/assays/imc.html
+                # "ROIStartXPosUm" and "ROIStartYPosUm"	Start X and Y-coordinates of the region of interest (µm).
+                # Note: This value must be divided by 1000 to correct for a bug (missing decimal point) in the Fluidigm software.
                 x1 = float(acquisition.metadata["ROIStartXPosUm"])/1000.0
                 y1 = float(acquisition.metadata["ROIStartYPosUm"])/1000.0
                 x2 = float(acquisition.metadata["ROIEndXPosUm"])
                 y2 = float(acquisition.metadata["ROIEndYPosUm"])
-
-
-
 
                 x_min_acq = min(x1, x2)
                 x_max_acq = max(x1, x2)
@@ -137,20 +142,20 @@ def plot_channel_onto_panorama(mcd_file,
                 if min_x < x_max_acq < max_x and min_y < y_max_acq < max_y:
                     pass
                 else:
-                    print("out of bounds")
+                    logger.warning(f"Acquisition {acquisition_idx} is outside of the panorama bounds, will ignore")
+                    logger.warning(f"Bounding box of acquisition: {x_min_acq}, {x_max_acq}, {y_min_acq}, {y_max_acq}")
+                    logger.warning(f"Bounding box of panorama: {min_x}, {max_x}, {min_y}, {max_y}")
                     continue
 
-                print(x_min_acq, x_max_acq, y_min_acq, y_max_acq)
                 x_pixel, y_pixel = int((x_min_acq-min_x) / x_um_per_pixel), int((max_y-y_max_acq) / y_um_per_pixel)
                 x_extent, y_extent = int((x_max_acq - x_min_acq) / x_um_per_pixel), int((y_max_acq - y_min_acq) / y_um_per_pixel)
-                print(x_pixel, y_pixel, x_extent, y_extent)
 
                 overlay = Image.fromarray(acquisition_arr).convert("L")
 
                 # Resize the overlay image
                 overlay = overlay.resize((x_extent, y_extent))
 
-                overlay = greyscale_to_colored(overlay)
+                overlay = greyscale_to_colored(overlay, color=overlay_color)
 
                 # Prepare a blank (alpha 0) image with the same size as the background
                 temp = Image.new('RGBA', background.size, (0, 0, 0, 0))
@@ -158,16 +163,15 @@ def plot_channel_onto_panorama(mcd_file,
                 # Paste the resized overlay onto the temporary image
                 temp.paste(overlay, (x_pixel, y_pixel))
 
-                temp.save("/tmp/temp.png")
+                temp.save(os.path.join(output_dir, f"slide{slide_idx}_acquisition{acquisition_idx}_{channel_label}.png"))
 
                 # Composite the temporary image onto the background
                 combined = Image.alpha_composite(combined, temp)
 
-    combined.save(output_file)
+        combined.save(os.path.join(output_dir, f"panorama_overlay_{slide_idx}_{channel_label}.png"))
 
 
 def greyscale_to_colored(greyscale_image, color=(57, 255, 20)):
-
     arr = np.array(greyscale_image)
     alpha_channel = arr  # Alpha from grayscale image
     rgba_overlay = np.zeros((arr.shape[0], arr.shape[1], 4), dtype=np.uint8)
